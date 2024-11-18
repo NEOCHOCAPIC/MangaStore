86c13bf968885839d9cf3d2a5aecbff400cd327b
NEOCHOCAPIC <juan.santibanezc@gmail.com>
2024-11-18 00:09:20 -0300

    prueba-Despligue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercelServerless from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: "server",
+  integrations: [tailwind(),
+    vercelServerless()
+  ]
 });
